fc8066269c0b62b7dd94bff4c10c7d8390adfc97
송다인 <inhere31@gmail.com>
2022-11-21 00:14:44 +0900

    송다인 reducer api

---
+++ src/api/MessageAPICalls.js
@@ -0,0 +1,352 @@
+import { GET_EMPLOYEE, POST_SEND_MESSAGE, 
+        GET_RECEIVE_MESSAGES, PATCH_RECEIVE_MESSAGE, GET_SEARCH_RECEIVE_MESSAGES,
+        GET_SEND_MESSAGES, GET_SEND_MESSAGE, GET_SEARCH_SEND_MESSAGES,
+        GET_IMPO_MESSAGES, PATCH_IMPO_MESSAGE, GET_SEARCH_IMPO_MESSAGE,
+        GET_RECEIVE_BIN_MESSAGES, GET_SEND_BIN_MESSAGES,
+        PATCH_RECIPIENT_MANAGEMENT, PATCH_SENDER_MANAGEMENT } from '../modules/MessageModule';
+
+export const callEmpListAPI = ({form}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/send/findEmp`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "GET",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept" : "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            },
+            body : JSON.stringify({
+                depNo : form.depNo
+            })
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: GET_EMPLOYEE, payload: result.data });
+        }
+    }
+}
+
+export const callSendMessageAPI = ({form}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/send`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept" : "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            },
+            body : JSON.stringify({
+                messageContent : form.messageContent,
+                recipient : {
+                    employeeNo : form.recipient.employeeNo
+                }
+            })
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: POST_SEND_MESSAGE, payload: result.data });
+        }
+    }
+}
+
+export const callReceiveMessageListAPI = ({currentPage = 1}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/receive?page=${currentPage}`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "GET",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept": "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: GET_RECEIVE_MESSAGES, payload: result.data });
+        }
+    }
+}
+
+export const callReceiveMessageReadAPI = ({messageNo}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/receive/${messageNo}/read`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "PATCH",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept": "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: PATCH_RECEIVE_MESSAGE, payload: result.data });
+        }
+    }
+}
+
+export const callSearchReceiveMessageAPI = ({keyword, currentPage = 1}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/receive/search?keyword=${keyword}&page=${currentPage}`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "GET",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept": "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: GET_SEARCH_RECEIVE_MESSAGES, payload: result.data });
+        }
+    }
+}
+
+export const callSendMessageListAPI = ({currentPage = 1}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/send?page=${currentPage}`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "GET",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept": "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: GET_SEND_MESSAGES, payload: result.data });
+        }
+    }
+}
+
+export const callSendMessageReadAPI = ({messageNo}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/send/${messageNo}/read`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "GET",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept": "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: GET_SEND_MESSAGE, payload: result.data });
+        }
+    }
+}
+
+export const callSearchSendMessageAPI = ({keyword, currentPage = 1}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/send/search?keyword=${keyword}&page=${currentPage}`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "GET",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept": "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: GET_SEARCH_SEND_MESSAGES, payload: result.data });
+        }
+    }
+}
+
+export const callImpoMessageListAPI = ({currentPage = 1}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/impo?page=${currentPage}`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "GET",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept": "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: GET_IMPO_MESSAGES, payload: result.data });
+        }
+    }
+}
+
+export const callImpoMessageReadAPI = ({messageNo}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/impo/${messageNo}/read`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "PATCH",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept": "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: PATCH_IMPO_MESSAGE, payload: result.data });
+        }
+    }
+}
+
+export const callSearchImpoMessageAPI = ({keyword, currentPage = 1}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/impo/search?keyword=${keyword}&page=${currentPage}`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "GET",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept": "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: GET_SEARCH_IMPO_MESSAGE, payload: result.data });
+        }
+    }
+}
+
+export const callBinReceiveMessageAPI = ({currentPage = 1}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/bin/receive?page=${currentPage}`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "GET",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept": "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: GET_RECEIVE_BIN_MESSAGES, payload: result.data });
+        }
+    }
+}
+
+export const callBinSendMessageAPI = ({currentPage = 1}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/bin/send?page=${currentPage}`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "GET",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept": "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            }
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: GET_SEND_BIN_MESSAGES, payload: result.data });
+        }
+    }
+}
+
+export const callRecipientManagementAPI = ({form, messageNo}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/receive/${messageNo}`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "PATCH",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept" : "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            },
+            body : JSON.stringify({
+                receiveMessageCategory : form.receiveMessageCategory,
+                receiveMessageDelete : form.receiveMessageDelete
+            })
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: PATCH_RECIPIENT_MANAGEMENT, payload: result.data });
+        }
+    }
+}
+
+export const callSenderManagementAPI = ({form, messageNo}) => {
+
+    const requestURL = `http://${process.env.REACT_APP_RESTAPI_IP}:8001/emp/message/send/${messageNo}`;
+
+    return async (dispatch, getState) => {
+
+        const result = await fetch(requestURL, {
+            method : "PATCH",
+            headers : {
+                "Content-Type" : "application/json",
+                "Accept" : "*/*",
+                "Authorization" : "Bearer " + window.localStorage.getItem('accessToken')
+            },
+            body : JSON.stringify({
+                sendMessageDelete : form.sendMessageDelete
+            })
+        })
+        .then(response => response.json());
+
+        if(result.status === 200) {
+            dispatch({ type: PATCH_SENDER_MANAGEMENT, payload: result.data });
+        }
+    }
+}
---
+++ src/modules/MessageModule.js
@@ -0,0 +1,106 @@
+import { createActions, handleActions } from "redux-actions";
+
+const initialState = [];
+
+export const GET_EMPLOYEE = 'message/GET_EMPLOYEE';
+export const POST_SEND_MESSAGE = 'message/POST_SEND_MESSAGE';
+
+export const GET_RECEIVE_MESSAGES = 'message/GET_RECEIVE_MESSAGES';
+export const PATCH_RECEIVE_MESSAGE = 'message/PATCH_RECEIVE_MESSAGE';
+export const GET_SEARCH_RECEIVE_MESSAGES = 'message/GET_SEARCH_RECEIVE_MESSAGES';
+
+export const GET_SEND_MESSAGES = 'message/GET_SEND_MESSAGES';
+export const GET_SEND_MESSAGE = 'message/GET_SEND_MESSAGE';
+export const GET_SEARCH_SEND_MESSAGES = 'message/GET_SEARCH_SEND_MESSAGES';
+
+export const GET_IMPO_MESSAGES = 'message/GET_IMPO_MESSAGES';
+export const PATCH_IMPO_MESSAGE = 'message/PATCH_IMPO_MESSAGE';
+export const GET_SEARCH_IMPO_MESSAGE = 'message/GET_SEARCH_IMPO_MESSAGE';
+
+export const GET_RECEIVE_BIN_MESSAGES = 'message/GET_RECEIVE_BIN_MESSAGES';
+export const GET_SEND_BIN_MESSAGES = 'message/GET_SEND_BIN_MESSAGES';
+
+export const PATCH_RECIPIENT_MANAGEMENT = 'message/PATCH_RECIPIENT_MANAGEMENT';
+export const PATCH_SENDER_MANAGEMENT = 'message/PATCH_SENDER_MANAGEMENT';
+
+const actions = createActions({
+    [GET_EMPLOYEE]: () => {},
+    [POST_SEND_MESSAGE]: () => {},
+
+    [GET_RECEIVE_MESSAGES]: () => {},
+    [PATCH_RECEIVE_MESSAGE]: () => {},
+    [GET_SEARCH_RECEIVE_MESSAGES]: () => {},
+
+    [GET_SEND_MESSAGES]: () => {},
+    [GET_SEND_MESSAGE]: () => {},
+    [GET_SEARCH_SEND_MESSAGES]: () => {},
+
+    [GET_IMPO_MESSAGES]: () => {},
+    [PATCH_IMPO_MESSAGE]: () => {},
+    [GET_SEARCH_IMPO_MESSAGE]: () => {},
+
+    [GET_RECEIVE_BIN_MESSAGES]: () => {},
+    [GET_SEND_BIN_MESSAGES]: () => {},
+
+    [PATCH_RECIPIENT_MANAGEMENT]: () => {},
+    [PATCH_SENDER_MANAGEMENT]: () => {},
+});
+
+const messageReducer = handleActions({
+
+    [GET_EMPLOYEE] : (state, { payload }) => {
+        return payload;
+    },
+    [POST_SEND_MESSAGE] : (state, { payload }) => {
+        return payload;
+    },
+
+    [GET_RECEIVE_MESSAGES] : (state, { payload }) => {
+        return payload;
+    },
+    [PATCH_RECEIVE_MESSAGE] : (state, { payload }) => {
+        return payload;
+    },
+    [GET_SEARCH_RECEIVE_MESSAGES] : (state, { payload }) => {
+        return payload;
+    },
+
+    [GET_SEND_MESSAGES] : (state, { payload }) => {
+        return payload;
+    },
+    [GET_SEND_MESSAGE] : (state, { payload }) => {
+        return payload;
+    },
+    [GET_SEARCH_SEND_MESSAGES] : (state, { payload }) => {
+        return payload;
+    },
+
+    [GET_IMPO_MESSAGES] : (state, { payload }) => {
+        return payload;
+    },
+    [PATCH_IMPO_MESSAGE] : (state, { payload }) => {
+        return payload;
+    },
+    [GET_SEARCH_IMPO_MESSAGE] : (state, { payload }) => {
+        return payload;
+    },
+
+    [GET_RECEIVE_BIN_MESSAGES] : (state, { payload }) => {
+        return payload;
+    },
+    [GET_SEND_BIN_MESSAGES] : (state, { payload }) => {
+        return payload;
+    },
+
+    [PATCH_RECIPIENT_MANAGEMENT] : (state, { payload }) => {
+        return payload;
+    },
+    [PATCH_SENDER_MANAGEMENT] : (state, { payload }) => {
+        return payload;
+    }
+    },
+    initialState
+);
+
+
+export default messageReducer;
