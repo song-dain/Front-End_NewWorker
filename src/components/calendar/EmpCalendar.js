7a4d63ee3f091ff74e4b5b8104af687c6d4203f5
송다인 <inhere31@gmail.com>
2022-11-25 10:43:25 +0900

    calendar 조회

--- src/components/calendar/EmpCalendar.js
+++ src/components/calendar/EmpCalendar.js
@@ -3,12 +3,26 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 
 class EmpCalendar extends Component {
+
     render() {
+
+      const onClickTest = () => {
+        console.log('이벤트 테스트')
+      }
+
         return (
           <div className="App">
             <FullCalendar 
               defaultView="dayGridMonth" 
               plugins={[ dayGridPlugin ]}
+              events={[
+                {
+                  title: 'test event', 
+                  start: '2022-11-24',
+                  end: '2022-11-26'
+                }
+              ]}
+              eventClick={ onClickTest }
             />
           </div>
         );
